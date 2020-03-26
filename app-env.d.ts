426f4115fbdf95e084774dda53b6f29c2ccb9c17
ToanVQ <toan.vuquoc@toshiba-tsdv.com>
2020-03-26 16:50:24 +0700

    init

---
+++ app-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="@types/node" />
+/// <reference types="@types/express" />
+
+interface ErrorException extends Error {
+	errno?: number;
+	code?: string;
+	path?: string;
+	syscall?: string;
+	stack?: string;
+}
---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import createError from 'http-errors';
+import express, { NextFunction, Response, Request } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logMiddleware from 'morgan';
+import http from 'http';
+import normalizePort from 'helpers/normalizePort';
+import logger from 'helpers/logger';
+
+const app = express();
+
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+app.use(logMiddleware('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({
+	extended: false
+}));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function (err: Error, req: Request<any>, res: Response<any>, next: NextFunction) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	// @ts-ignore
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+server.on('error', (error: ErrorException) => {
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+
+	var bind = typeof port === 'string'
+		? 'Pipe ' + port
+		: 'Port ' + port;
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+});
+
+server.on('listening', () => {
+	var addr = server.address();
+	var bind = typeof addr === 'string'
+		? 'pipe ' + addr
+		: 'port ' + addr?.port ?? 80;
+	logger.info('Listening on ' + bind);
+});
+
+server.listen(port);
